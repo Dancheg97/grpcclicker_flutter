9526b7e575f3e236eb0ab756b33ff81ee829d262
Danila Fomin <dangdancheg@gmail.com>
2022-05-21 20:30:12 +0300

    implemented movewindow on left side

--- lib/main.dart
+++ lib/main.dart
@@ -44,6 +44,9 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
       home: Scaffold(
         body: WindowBorder(
           color: Colors.black,
